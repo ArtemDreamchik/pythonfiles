ae88262b976dd1c6a4dc5d06891963c5515a6685
Artem <akkmine3@gmail.com>
2024-12-04 19:15:03 +0300

    1ver

---
+++ main.py
@@ -0,0 +1,12 @@
+from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
+import uvicorn
+
+app = FastAPI()
+
+@app.get('/', response_class=HTMLResponse)
+async def greet(name: str = 'Recruto', message: str = 'Давай дружить'):
+    return f"<h1> Hello {name}! {message}!</h1>"
+
+if __name__ == '__main__':
+    uvicorn.run('main:app')
